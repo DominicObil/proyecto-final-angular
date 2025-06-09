0b99ed72a6af8711186578adc154f89fcb0ebec3
DominicObil <dobioso519@g.educaand.es>
2025-06-10 00:57:30 +0200

    arreglado6

---
+++ src/app/app.config.server.ts
@@ -0,0 +1,13 @@
+import { provideServerRendering } from '@angular/ssr';
+import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { appConfig } from './app.config';
+
+// Elimina esto:
+// import { serverRoutes } from './app.routes.server';
+
+// Y cámbialo así:
+const serverConfig: ApplicationConfig = {
+  providers: [provideServerRendering()]
+};
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
